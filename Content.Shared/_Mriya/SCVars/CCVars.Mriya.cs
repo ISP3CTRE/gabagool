a6aa5c11030103ff5004e7433a1431e535f0c014
Oleksandr <aleksandr.vernigora93@gmail.com>
2026-07-11 17:59:00 +0300

    UPdate species speech (#41)
    * fix moth speechSound
    
    * add bububu
    
    * add cvar for hot swap method for speech
    
    * Prevent bubububu spam
    
    * fix cvar

--- Content.Shared/_Mriya/SCVars/CCVars.Mriya.cs
+++ Content.Shared/_Mriya/SCVars/CCVars.Mriya.cs
@@ -34,4 +34,16 @@ public sealed partial class CCVars
     /// </summary>
     public static readonly CVarDef<string> DiscordStartRoundStatsWebhookColor =
         CVarDef.Create("discord.start_round_stats_webhook_embed_color", Color.LawnGreen.ToHex(), CVar.SERVERONLY);
+
+    /// <summary>
+    ///     Set to true to enable Mriya's speech "bububu" sound effect when a player speaks.
+    /// </summary>
+    public static readonly CVarDef<bool> MriyaSpeechBububu =
+        CVarDef.Create("mriya.speech_bububu_enabled", true, CVar.SERVERONLY);
+
+    /// <summary>
+    ///     Set max number of words in a message to trigger Mriya's speech "bububu" sound effect.
+    /// </summary>
+    public static readonly CVarDef<int> MriyaSpeechBububuMaxWord =
+        CVarDef.Create("mriya.speech_bububu_max_words", 10, CVar.SERVERONLY);
 }
